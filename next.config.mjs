ac4cc99c914d122178f7774322ea83e4004c5ba9
HashirAligb <HASHIRALI.ALI21@qmail.cuny.edu>
2025-03-16 00:49:49 -0400

    Fix deployment configuration for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ try {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'dist',
   images: {
     unoptimized: true,
   },
@@ -22,6 +23,7 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
   basePath: '',
   trailingSlash: true,
 }
