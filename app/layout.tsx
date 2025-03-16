bd26872d6f0240129b5382f05320d5024511b68d
HashirAligb <HASHIRALI.ALI21@qmail.cuny.edu>
2025-03-16 16:54:26 -0400

    Update project name and title to 'Hashir Ali | Portfolio'

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Hashir Ali | Portfolio',
+  description: 'Personal portfolio website of Hashir Ali - Computer Science student and software developer',
   generator: 'v0.dev',
 }
 
